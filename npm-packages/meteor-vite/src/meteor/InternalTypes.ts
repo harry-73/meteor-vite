4b4fea90dc9f18ae9ccdeb007fc64c2b3955d227
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-16 20:42:57 +0300

    Add module for undeclared internal Meteor types.

---
+++ npm-packages/meteor-vite/src/meteor/InternalTypes.ts
@@ -0,0 +1,29 @@
+export type MeteorProgram = {
+    type: 'web-program-pre1'
+    manifest: MeteorManifest[]
+}
+export type MeteorManifest = {
+    path: string;
+    where: 'client' | 'internal' | string;
+    type: 'js' | 'css' | 'body' | 'head' | string;
+    cacheable: boolean;
+    url: string;
+    size: number;
+    hash: string;
+    sri: string;
+    replaceable: boolean;
+    sourceMap: string;
+    sourceMapUrl: string;
+}
+
+export type MeteorRuntimeConfig = {
+    reactFastRefreshEnabled: boolean;
+    isModern: boolean;
+    gitCommitHash: string;
+    ROOT_URL: string;
+    meteorEnv: { NODE_ENV: string; TEST_METADATA: string };
+    _hmrSecret: string;
+    ROOT_URL_PATH_PREFIX: string;
+    meteorRelease: string;
+    PUBLIC_SETTINGS: { [key: string]: unknown }
+}
