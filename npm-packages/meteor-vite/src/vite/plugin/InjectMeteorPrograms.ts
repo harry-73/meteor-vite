099f12ba6a9a7916488a4a047dde5105f97b3801
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-17 19:26:45 +0300

    Only run Meteor program injection plugin in viteMode: ssr

--- npm-packages/meteor-vite/src/vite/plugin/InjectMeteorPrograms.ts
+++ npm-packages/meteor-vite/src/vite/plugin/InjectMeteorPrograms.ts
@@ -1,18 +1,26 @@
 import FS from 'fs/promises';
 import { HtmlTagDescriptor } from 'vite';
 import { MeteorManifest, MeteorProgram } from '../../meteor/InternalTypes';
+import { MeteorViteConfig } from '../MeteorViteConfig';
 import { PluginSettings } from './MeteorStubs';
 import { Plugin } from 'vite';
 import Path from 'path';
 
-export default function InjectMeteorPrograms(pluginSettings:  PluginSettings) {
+export default function InjectMeteorPrograms(pluginSettings:  Pick<PluginSettings, 'meteor'>) {
+    let resolvedConfig: MeteorViteConfig;
     return {
         name: 'meteor-vite: inject Meteor Programs HTML',
+        configResolved(config) {
+            resolvedConfig = config;
+        },
+        
         /**
          * When acting as the frontend server in place of Meteor, inject Meteor's package import scripts into the
          * server-rendered page.
          */
         async transformIndexHtml() {
+            if (resolvedConfig.meteor?.viteMode !== 'ssr') return;
+            
             const path = Path.join(pluginSettings.meteor.packagePath, '../program.json');
             const program: MeteorProgram = JSON.parse(await FS.readFile(path, 'utf-8'));
             let imports: HtmlTagDescriptor[] = [];
