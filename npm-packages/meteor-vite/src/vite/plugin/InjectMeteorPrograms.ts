490c45bd7b35c5bf95a282c0cc86bea240047626
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-18 01:55:46 +0300

    Import Meteor code directly into Vite instead of injecting references into HTML. This should enable Vite to do SSR for Meteor. 🤞

--- npm-packages/meteor-vite/src/vite/plugin/InjectMeteorPrograms.ts
+++ npm-packages/meteor-vite/src/vite/plugin/InjectMeteorPrograms.ts
@@ -1,17 +1,62 @@
 import FS from 'fs/promises';
-import { HtmlTagDescriptor } from 'vite';
-import { MeteorManifest, MeteorProgram } from '../../meteor/InternalTypes';
+import { MeteorProgram } from '../../meteor/InternalTypes';
 import { MeteorViteConfig } from '../MeteorViteConfig';
 import { PluginSettings } from './MeteorStubs';
 import { Plugin } from 'vite';
 import Path from 'path';
 
-export default function InjectMeteorPrograms(pluginSettings:  Pick<PluginSettings, 'meteor'>) {
+
+export default async function InjectMeteorPrograms(pluginSettings:  Pick<PluginSettings, 'meteor'>) {
     let resolvedConfig: MeteorViteConfig;
+    
+    const path = Path.join(pluginSettings.meteor.packagePath, '../program.json');
+    const program: MeteorProgram = JSON.parse(await FS.readFile(path, 'utf-8'));
+    const virtualImports: string[] = []
+    program.manifest.forEach((entry) => {
+        if (entry.type === 'js') {
+            virtualImports.push(`import '\0${Path.join(path, '../', entry.path).replace(/^\/+/, '')}';`)
+        }
+    })
+    
     return {
         name: 'meteor-vite: inject Meteor Programs HTML',
         configResolved(config) {
             resolvedConfig = config;
+        },
+        
+        resolveId(id) {
+            if (id.startsWith('.meteor')) {
+                return `\0${id}`
+            }
+            if (id.startsWith('virtual:meteor-bundle')) {
+                return `\0${id}`
+            }
+            if (id.startsWith('\0.meteor')) {
+                return id;
+            }
+        },
+        
+        /**
+         * Create a virtual meteor bundle for directly pulling in Meteor code into Vite.
+         * This is done primarily for instances where Vite is acting as the sole user-facing server. E.g. when doing
+         * SSR through Vite and Meteor is only used as a real-time API server.
+         */
+        async load(id) {
+            id = id.slice(1);
+            if (id.startsWith('virtual:meteor-bundle')) {
+                return virtualImports.join('\n');
+            }
+            if (!id.startsWith('.meteor')) {
+                return;
+            }
+            const filePath = Path.join(process.cwd(), id);
+            const content = await FS.readFile(filePath, 'utf-8');
+            if (id.endsWith('global-imports.js')) {
+                const newContent = content.split(/[\r\n]/).map((line) => line.replace(/^(\w+) =/, 'globalThis.$1 =')).join('\n');
+                return newContent;
+            }
+            return content.replace(/global = this/g, 'global = globalThis')
+                          .replace(/^([\w]+) =/gi, 'globalThis.$1 =');
         },
         
         /**
