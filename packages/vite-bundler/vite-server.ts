5bd63b7682d50876cd29d71f57c8b0ec8cc067e3
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-17 18:55:13 +0300

    Fix type errors in vite-server.ts

--- packages/vite-bundler/vite-server.ts
+++ packages/vite-bundler/vite-server.ts
@@ -34,7 +34,7 @@ if (Meteor.isDevelopment) {
         res.setHeader('Content-Type', 'application/javascript')
         res.writeHead(200);
         
-        const meteorRuntimeConfig: MeteorRuntimeConfig = JSON.parse(WebApp.clientPrograms['web.browser'].meteorRuntimeConfig);
+        const meteorRuntimeConfig: MeteorRuntimeConfig = getRuntimeConfig('web.browser');
         const config = Object.assign({}, { DDP_DEFAULT_CONNECTION_URL: meteorRuntimeConfig.ROOT_URL }, meteorRuntimeConfig);
         
         res.end(`__meteor_runtime_config__ = JSON.parse(decodeURIComponent(${WebApp.encodeRuntimeConfig(config)}));`);
@@ -110,6 +110,11 @@ function createViteServer() {
     return viteServer;
 }
 
+function getRuntimeConfig(arc: 'web.browser'): MeteorRuntimeConfig {
+    const program = WebApp.clientPrograms[arc] as typeof WebApp.clientPrograms[string] & { meteorRuntimeConfig: string };
+    return JSON.parse(program.meteorRuntimeConfig);
+}
+
 function createMeteorViteBundleWatcher() {
     const packageBuilder = createWorkerFork({});
     let restartTimeout: ReturnType<typeof setTimeout>;
@@ -151,4 +156,10 @@ interface BoilerplateData {
     dynamicBody?: string;
     additionalStaticJs: [contents: string, pathname: string][];
     inline?: string;
+}
+
+declare module 'meteor/webapp' {
+    module WebApp {
+        function encodeRuntimeConfig(config: object): string;
+    }
 }
