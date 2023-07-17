26430f7d96b26a9521daaacc6f7c641437973d6f
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-17 14:32:58 +0300

    Add vite-plugin-ssr render boilerplate

---
+++ examples/vue-ssr/imports/ui/ssr-render/_default.page.client.ts
@@ -0,0 +1,16 @@
+export { render }
+
+import { createApp } from './App'
+import type { PageContextClient } from './Types'
+
+// This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
+async function render(pageContext: PageContextClient) {
+    const { Page, pageProps } = pageContext
+    if (!Page) throw new Error('Client-side render() hook expects pageContext.Page to be defined')
+    const app = createApp(Page, pageProps, pageContext)
+    app.mount('#app')
+}
+
+/* To enable Client-side Routing:
+export const clientRouting = true
+// !! WARNING !! Before doing so, read https://vite-plugin-ssr.com/clientRouting */
