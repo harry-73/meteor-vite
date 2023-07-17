04e4a1cd0cdd956f738694e7816f4b48713af84d
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-17 14:41:26 +0300

    Specify port in Vite config.

--- examples/vue-ssr/vite.config.ts
+++ examples/vue-ssr/vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { ssr } from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
+    server: {
+        port: 5173,
+    },
+    
     plugins: [
         vue(),
         ssr(),
