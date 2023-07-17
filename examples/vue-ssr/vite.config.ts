30d0b45984238b5c2e64f3284a07878fc2bd7c3f
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-17 14:39:27 +0300

    Add vite-plugin-ssr to vue-ssr example

--- examples/vue-ssr/vite.config.ts
+++ examples/vue-ssr/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 // Example with Vue
 import vue from '@vitejs/plugin-vue'
+import { ssr } from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
     plugins: [
         vue(),
+        ssr(),
     ],
     
     meteor: {
