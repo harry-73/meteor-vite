4766aa53eed58ca9832efcada79178335571172a
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-18 10:16:57 +0300

    Add quick Meteor chat to ClientContent.vue

--- examples/vue-ssr/imports/ui/pages/meteor/ClientContent.vue
+++ examples/vue-ssr/imports/ui/pages/meteor/ClientContent.vue
@@ -7,8 +7,27 @@
     <h5>Last session ID</h5>
     <pre>{{ Meteor.connection.lastSessionId }}</pre>
   </div>
+  <form @submit.prevent="send">
+    <h5>Meteor Chat</h5>
+    <pre>
+      <span style="display: block" v-for="{message} in messages">{{ message }}</span>
+    </pre>
+    <input type="text" v-model="message">
+  </form>
 </template>
 
 <script lang="ts" setup>
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { Methods, Collection, Publications } from '../../../api/chat/index';
+import { ref } from 'vue';
+const message = ref('');
+const messages = ref([]);
+function send() {
+    Methods.send(message);
+}
+Tracker.autorun(() => {
+    const sub = Publications.all();
+    messages.value = Collection.find().fetch();
+})
 </script>
