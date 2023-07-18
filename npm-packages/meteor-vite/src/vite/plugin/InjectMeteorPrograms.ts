a1b30dd638a2b41d3ec96ce2af26a5b97c1151e3
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-18 04:29:57 +0300

    Remove HTML program injection in favor of runtime imports.

--- npm-packages/meteor-vite/src/vite/plugin/InjectMeteorPrograms.ts
+++ npm-packages/meteor-vite/src/vite/plugin/InjectMeteorPrograms.ts
@@ -64,36 +64,6 @@ export default async function InjectMeteorPrograms(pluginSettings:  Pick<PluginS
                           .replace(/^([\w]+) =/gi, 'globalThis.$1 =');
         },
         
-        /**
-         * When acting as the frontend server in place of Meteor, inject Meteor's package import scripts into the
-         * server-rendered page.
-         */
-        async transformIndexHtml() {
-            if (resolvedConfig.meteor?.viteMode !== 'ssr') return;
-            
-            const path = Path.join(pluginSettings.meteor.packagePath, '../program.json');
-            const program: MeteorProgram = JSON.parse(await FS.readFile(path, 'utf-8'));
-            let imports: HtmlTagDescriptor[] = [];
-            
-            const assetUrl = (manifest: MeteorManifest) => {
-                const base = pluginSettings.meteor.runtimeConfig.ROOT_URL.replace(/^\/*/, '');
-                const path = manifest.url.replace(/^\/*/, '');
-                return `${base}${path}`;
-            }
-            
-            imports.push({ tag: 'script', attrs: { type: 'text/javascript', src: 'http://localhost:3000/__meteor_runtime_config.js' } })
-            
-            program.manifest.forEach((asset) => {
-                if (asset.type === 'css') {
-                    imports.push({ tag: 'link', injectTo: 'head', attrs: { href: assetUrl(asset), rel: 'stylesheet' } })
-                }
-                if (asset.type === 'js') {
-                    imports.push({ tag: 'script', injectTo: 'head', attrs: { type: 'text/javascript', src: assetUrl(asset) } })
-                }
-            })
-            
-            return imports;
-        },
     } satisfies Plugin;
 }
 
