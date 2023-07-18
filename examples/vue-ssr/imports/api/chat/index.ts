5f04672587f62ebbc5942acab6da374715621e79
Jørgen Vatle <Jorgen@WcAServices.net>
2023-07-18 10:11:08 +0300

    Add chat API

---
+++ examples/vue-ssr/imports/api/chat/index.ts
@@ -0,0 +1,39 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+export const CollectionName = 'chat';
+export const Collection = new Mongo.Collection(CollectionName);
+const Methods = {
+    send(message: unknown) {
+        if (Meteor.isServer) {
+            if (typeof message !== 'string') {
+                throw new Meteor.Error('Your message needs to be a plain string!');
+            }
+            return Collection.insert({ message });
+        }
+        
+        Meteor.call(`${CollectionName}.send`, message, (err) => {
+            if (err) {
+                throw err;
+            }
+        })
+    }
+};
+export const Publications = {
+    'all'() {
+        if (Meteor.isServer) {
+            return Collection.find();
+        }
+        
+        return Meteor.subscribe(`${CollectionName}.all`);
+    }
+}
+
+if (Meteor.isServer) {
+    Meteor.methods(
+        Object.fromEntries(Object.entries(Methods).map(([method, handler]) => [`${CollectionName}.${method}`, handler]))
+    )
+    Object.entries(Publications).forEach(([name, handler]) => {
+        Meteor.publish(name, handler);
+    })
+}
