35f477dfbeb72c64a7a4d0218cb774a8fa70b834
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-20 20:01:58 +0300

    Refactor production build runner to its own function.

--- npm-packages/meteor-vite/src/bin/worker/build-for-production.ts
+++ npm-packages/meteor-vite/src/bin/worker/build-for-production.ts
@@ -1,6 +1,5 @@
-import Path from 'path';
 import { RollupOutput } from 'rollup';
-import { build, resolveConfig } from 'vite';
+import { build, PluginOption, resolveConfig } from 'vite';
 import { MeteorViteConfig } from '../../vite/MeteorViteConfig';
 import { MeteorStubs } from '../../vite';
 import MeteorVitePackage from '../../../package.json';
@@ -25,10 +24,16 @@ type Replies = IPCReply<{
             build: {
                 outDir: string;
             }
-            output?: {name?: string, type: string, fileName: string}[]
+            output?: BuildOutput;
         };
     }
 }>
+
+type BuildOutput = {
+    name?: string,
+    type: string,
+    fileName: string,
+}[];
 
 export default CreateIPCInterface({
     async buildForProduction(
@@ -47,54 +52,11 @@ export default CreateIPCInterface({
             configFile: resolveConfigFilePath(packageJson),
         }, 'build');
         
-        if (!viteConfig.meteor?.clientEntry) {
-            throw new Error(`You need to specify an entrypoint in your Vite config! See: ${MeteorVitePackage.homepage}`);
-        }
         
-        const results = await build({
-            configFile: viteConfig.configFile,
-            build: {
-                lib: {
-                    entry: viteConfig?.meteor?.clientEntry,
-                    formats: ['es'],
-                },
-                rollupOptions: {
-                    output: {
-                        entryFileNames: 'meteor-entry.js',
-                        chunkFileNames: '[name].js',
-                    },
-                },
-                outDir: viteOutDir,
-                minify: false,
-            },
-            plugins: [
-                // Get fully resolved config to feed any potential changes back to the Meteor compiler.
-                // vite-plugin-ssr prefixes the output directory with the deployment target (client/server)
-                // Resulting in e.g. `output/vite` turning into `output/vite/client`
-                {
-                    name: 'meteor-vite: config resolver',
-                    configResolved: (config) => {
-                        viteConfig = config
-                    }
-                },
-                MeteorStubs({ meteor, packageJson, }),
-                InjectMeteorPrograms({ meteor }),
-            ],
-        });
-        
-        const result = Array.isArray(results) ? results[0] : results;
-        
-        function validateOutput(rollupResult: typeof result): asserts rollupResult is RollupOutput {
-            if ('output' in rollupResult) {
-                return;
-            }
-            
-            const message = 'Unexpected rollup result!';
-            console.error(message, rollupResult);
-            throw new Error(message);
-        }
-        
-        validateOutput(result);
+        const plugins = [
+            MeteorStubs({ meteor, packageJson, }),
+            InjectMeteorPrograms({ meteor }),
+        ]
         
         // Result payload
         reply({
@@ -106,15 +68,77 @@ export default CreateIPCInterface({
                     build: {
                         outDir: viteConfig.build.outDir,
                     },
-                    output: result.output.map(o => ({
-                        name: o.name,
-                        type: o.type,
-                        fileName: o.fileName,
-                    })),
+                    output: await runBuild({
+                        viteOutDir,
+                        viteConfig,
+                        plugins,
+                    }),
                 },
             }
         })
     }
 })
 
+
+async function runBuild({ viteConfig, viteOutDir, plugins }: {
+    viteConfig: MeteorViteConfig,
+    viteOutDir: string;
+    plugins: PluginOption[],
+}) {
+    let outDir = viteConfig.build.outDir;
+    
+    if (!viteConfig.meteor?.clientEntry) {
+        throw new Error(`You need to specify an entrypoint in your Vite config! See: ${MeteorVitePackage.homepage}`);
+    }
+    
+    const results = await build({
+        configFile: viteConfig.configFile,
+        build: {
+            lib: {
+                entry: viteConfig?.meteor?.clientEntry,
+                formats: ['es'],
+            },
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'meteor-entry.js',
+                    chunkFileNames: '[name].js',
+                },
+            },
+            outDir: viteOutDir,
+            minify: false,
+        },
+        plugins: [
+            // Get fully resolved config to feed any potential changes back to the Meteor compiler.
+            // vite-plugin-ssr prefixes the output directory with the deployment target (client/server)
+            // Resulting in e.g. `output/vite` turning into `output/vite/client`
+            {
+                name: 'meteor-vite: out-dir resolver',
+                configResolved: (config) => {
+                    outDir = config.build.outDir;
+                }
+            },
+            ...plugins,
+        ],
+    });
+    
+    const result = Array.isArray(results) ? results[0] : results;
+    
+    function validateOutput(rollupResult: typeof result): asserts rollupResult is RollupOutput {
+        if ('output' in rollupResult) {
+            return;
+        }
+        
+        const message = 'Unexpected rollup result!';
+        console.error(message, rollupResult);
+        throw new Error(message);
+    }
+    
+    validateOutput(result);
+    
+    return result.output.map(o => ({
+        name: o.name,
+        type: o.type,
+        fileName: o.fileName,
+    }))
+}
 
