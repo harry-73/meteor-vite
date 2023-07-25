587e171b3ed76d03600ba3af815a1299a398ecd7
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-25 15:21:04 +0300

    Use boilerplate hook for ssr rendering

--- examples/vue-ssr/server/ssr-server.ts
+++ examples/vue-ssr/server/ssr-server.ts
@@ -1,17 +1,33 @@
 import { renderPage } from 'vite-plugin-ssr/server';
-import { WebApp } from 'meteor/webapp';
+import { WebApp, WebAppInternals } from 'meteor/webapp';
 import { Meteor } from 'meteor/meteor'
 
 if (Meteor.isProduction) {
     import '/vite/vite-server/importBuild.cjs';
 }
 
-WebApp.connectHandlers.use('/', async (req, res, next) => {
+WebAppInternals.registerBoilerplateDataCallback('ssr-server', async (req, data) => {
+    const pageContextInit = {
+        urlOriginal: req.url.href
+    }
+    
+    console.log('Received request:', { pageContextInit });
+    
+    const { httpResponse } = await renderPage(pageContextInit)
+    
+    const { body, statusCode, contentType, earlyHints } = httpResponse
+    
+    data.dynamicBody = body;
+    
+});
+
+WebApp.connectHandlers.use('/ssr', async (req, res, next) => {
+    console.log(WebApp.clientPrograms['web.browser'])
     const pageContextInit = {
         urlOriginal: req.originalUrl
     }
     
-    console.log('Received request:', { pageContextInit });
+    console.log('Received request:', { pageContextInit,  });
     
     const { httpResponse } = await renderPage(pageContextInit)
     
