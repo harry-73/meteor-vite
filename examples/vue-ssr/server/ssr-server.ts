d004fc1872f66104e5f5436f2e15b83346c3660b
Jørgen Vatle <jorgen@wcaservices.net>
2023-07-25 03:40:49 +0300

    Reformat code style for ssr connect handler.

--- examples/vue-ssr/server/ssr-server.ts
+++ examples/vue-ssr/server/ssr-server.ts
@@ -10,11 +10,20 @@ WebApp.connectHandlers.use('/', async (req, res, next) => {
     const pageContextInit = {
         urlOriginal: req.originalUrl
     }
+    
     console.log('Received request:', { pageContextInit });
-    const pageContext = await renderPage(pageContextInit)
-    const { httpResponse } = pageContext
-    if (!httpResponse) return next()
+    
+    const { httpResponse } = await renderPage(pageContextInit)
+    
+    if (!httpResponse) {
+        return next()
+    }
+    
     const { body, statusCode, contentType, earlyHints } = httpResponse
-    if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+    
+    if (res.writeEarlyHints) {
+        res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+    }
+    
     res.setHeader('Content-Type', contentType).writeHead(statusCode).end(body);
 });
