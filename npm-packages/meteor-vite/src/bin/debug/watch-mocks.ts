23710faa021012df7cb12fd78f42c676ea3b40ed
Jørgen Vatle <jorgen@wcaservices.net>
2023-10-22 17:00:37 +0300

    Remove error formatting tests from watch-mocks.ts

--- npm-packages/meteor-vite/src/bin/debug/watch-mocks.ts
+++ npm-packages/meteor-vite/src/bin/debug/watch-mocks.ts
@@ -1,7 +1,5 @@
 import { Check, TestLazy, TsModules } from '../../../test/__mocks'
 import MeteorPackage from '../../meteor/package/components/MeteorPackage'
-import { MeteorViteError } from '../../vite/error/MeteorViteError'
-import ViteServer from './vite-server';
 
 /**
  * Parse test module on startup for debugging and development.
@@ -13,28 +11,8 @@ import ViteServer from './vite-server';
   for (const { filePath, fileContent } of mocks) {
     console.log(`${'--'.repeat(64)}`)
     const result = await MeteorPackage.parse({ filePath, fileContent })
-    console.log(result.modules)
+    console.log(result);
   }
-
-  ViteServer.then((server) => {
-    server.listen(4949)
-  })
-  class ViteJsonError extends MeteorViteError {
-    public async formatLog() {
-      this.addSection('some data', { ...this.package, ...this.context })
-    }
-  }
-  const error = new ViteJsonError('Test message 3', {
-    subtitle: 'This is a subtitle!',
-    package: {
-      packageId: 'test:ts-modules',
-    },
-    context: {
-      id: 'meteor/test:ts-modules/foo-bar.ts',
-    },
-  })
-  await error.beautify()
-  console.error(error)
 })()
 
 setInterval(() => 'Keeps the ts-node-debug process running for development', 100)
